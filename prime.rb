187b293f9854fe79adecfd59c6c2ab40e48b5b37
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-07-25 18:49:33 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(integer)
+  if integer > 1
+        range = (2..(integer - 1)).to_a
+        range.none? do |prime|
+        integer % prime == 0
+        end
+    else
+        false
+    end
+end
